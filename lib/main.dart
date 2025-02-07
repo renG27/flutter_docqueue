b60e12de996da6b372af67e6378ccdd671045ebb
RenG27 <genrves@gmail.com>
2025-02-07 16:05:17 +0700

    commit 1.2_phase 4

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:flutter_doctorappqueue/providers/queue_provider.dart';
 import 'package:flutter_doctorappqueue/screens/queue_screen.dart';
 import 'package:flutter_doctorappqueue/services/notification_service.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_doctorappqueue/screens/patient_details_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +14,27 @@ void main() async {
 
   final LocalNotificationService localNotificationService = LocalNotificationService();
   await localNotificationService.initialize();
-  await localNotificationService.requestPermissions(); // Request permissions
+  await localNotificationService.requestPermissions();
+
+  final GoRouter _router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const QueueScreen();
+        },
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'patient_details/:patientId',
+            builder: (BuildContext context, GoRouterState state) {
+              final patientId = state.pathParameters['patientId']!;
+              return PatientDetailsScreen(patientId: patientId);
+            },
+          ),
+        ],
+      ),
+    ],
+  );
 
   runApp(
     MultiProvider(
@@ -24,8 +46,12 @@ void main() async {
           ),
         ),
       ],
-      child: const MaterialApp(
-        home: QueueScreen(),
+      child: MaterialApp.router(  // Use MaterialApp.router
+        routerConfig: _router,   // Pass the router configuration
+        title: 'Doctor Patient Queue',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
       ),
     ),
   );
